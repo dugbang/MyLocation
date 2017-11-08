c89a1284b3eba7aa2319851df721583745de066a
dugbang <dugbang@gmail.com>
2017-11-08 19:49:39 +0900

    Initial commit

---
+++ app/src/main/java/com/example/shbae/mylocation/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.shbae.mylocation;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startLocationService();
+            }
+        });
+    }
+
+    private void startLocationService() {
+        long minTime = 10000;
+        float minDistance = 0;
+
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        manager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                minTime,
+                minDistance,
+                new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        Double latitude = location.getLatitude();
+                        Double longitude = location.getLongitude();
+
+                        textView.setText("내 위치; " + latitude + ", " + longitude);
+                    }
+
+                    @Override
+                    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String s) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String s) {
+
+                    }
+                }
+        );
+    }
+}
